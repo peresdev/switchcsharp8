1b366ac208784bd1b12364926f767ea981c70ad8
Leandro Peres Gonçalves <leandro.peres@dattos.com.br>
2020-10-29 15:45:06 -0300

    switch c# 8

---
+++ switchcsharp8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace switchcsharp8
+{
+    class Program
+    {
+        public enum Rainbow
+        {
+            Red,
+            Orange,
+            Yellow,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+
+        public static string FromRainbow(Rainbow colorBand) =>
+            colorBand switch
+            {
+                Rainbow.Red => "Red",
+                Rainbow.Orange => "Orange",
+                Rainbow.Yellow => "Yellow",
+                _ => "invalid color value: " + colorBand,
+            };
+
+        static void Main(string[] args) => Console.WriteLine("Color: " + FromRainbow(Rainbow.Blue));
+    }
+}
